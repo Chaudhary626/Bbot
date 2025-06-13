a315309fef92c737f5a34021d9f17e7565f1be83
MANISH CHAUDHARY <132375487+Chaudhary626@users.noreply.github.com>
2025-06-14 00:13:50 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+BOT_TOKEN = "7342216134:AAEl99PlNXo56Vl0DDuSXC04Wq49IGsb2cw"  # Replace with your actual Telegram bot token
+
+DB_NAME = "youtube_bot.db"         # SQLite database file
+
+MAX_VIDEO_LIMIT = 5               # Max videos a user can submit
+MAX_DURATION_MINUTES = 5          # Max video duration allowed in minutes
+PROOF_TIMEOUT_MINUTES = 15        # Time after which unverified proofs expire
